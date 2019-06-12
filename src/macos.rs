bcad3fb877e04536b07177d1e1b995a88c428d5d
Joseph Richey <joerichey@google.com>
2019-06-12 04:04:05 -0700

    macos: Cleanup SecRandom type (#28)

--- src/macos.rs
+++ src/macos.rs
@@ -13,27 +13,20 @@ use crate::Error;
 use std::io;
 use core::num::NonZeroU32;
 
-enum SecRandom {}
-
-/// Essentially a null pointer (type `SecRandomRef`)
-#[allow(non_upper_case_globals)]
-const kSecRandomDefault: *const SecRandom = 0 as *const SecRandom;
+// TODO: Make extern once extern_types feature is stabilized. See:
+//   https://github.com/rust-lang/rust/issues/43467
+#[repr(C)]
+struct SecRandom([u8; 0]);
 
 #[link(name = "Security", kind = "framework")]
 extern {
-    fn SecRandomCopyBytes(
-        rnd: *const SecRandom, count: libc::size_t, bytes: *mut u8,
-    ) -> libc::c_int;
+    static kSecRandomDefault: *const SecRandom;
+
+    fn SecRandomCopyBytes(rnd: *const SecRandom, count: usize, bytes: *mut u8) -> libc::c_int;
 }
 
 pub fn getrandom_inner(dest: &mut [u8]) -> Result<(), Error> {
-    let ret = unsafe {
-        SecRandomCopyBytes(
-            kSecRandomDefault,
-            dest.len() as libc::size_t,
-            dest.as_mut_ptr(),
-        )
-    };
+    let ret = unsafe { SecRandomCopyBytes(kSecRandomDefault, dest.len(), dest.as_mut_ptr()) };
     if ret == -1 {
         error!("SecRandomCopyBytes call failed");
         Err(io::Error::last_os_error().into())
