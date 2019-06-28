eb726fa0595b1ffb2d36d62bc0cbea257c48c10f
Joe Richey <joerichey@google.com>
2019-06-28 15:35:41 -0700

    Move macOS/iOS implementaiton to ios.rs

--- src/ios.rs
+++ src/ios.rs
@@ -6,7 +6,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-//! Implementation for MacOS / iOS
+//! Implementation for iOS
 extern crate std;
 
 use crate::Error;
