e01f169024ff280b699002b601f3a70a3890d2f2
Joe Richey <joerichey@google.com>
2019-07-01 16:14:35 -0700

    openBSD: Make imports consistent

--- src/openbsd_bitrig.rs
+++ src/openbsd_bitrig.rs
@@ -10,8 +10,8 @@
 extern crate std;
 
 use crate::Error;
+use core::num::NonZeroU32;
 use std::io;
-use std::num::NonZeroU32;
 
 pub fn getrandom_inner(dest: &mut [u8]) -> Result<(), Error> {
     for chunk in dest.chunks_mut(256) {
