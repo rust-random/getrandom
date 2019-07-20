859d3dc8fc7d67f22dda3653484bf713e8aab752
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2019-07-20 03:14:26 +0300

    use wasi-core isntead of libc

--- src/wasi.rs
+++ src/wasi.rs
@@ -9,12 +9,12 @@
 //! Implementation for WASI
 use crate::Error;
 use core::num::NonZeroU32;
+use wasi_core::wasi_unstable::random_get;
 
 pub fn getrandom_inner(dest: &mut [u8]) -> Result<(), Error> {
-    let ret =
-        unsafe { libc::__wasi_random_get(dest.as_mut_ptr() as *mut libc::c_void, dest.len()) };
+    let ret = random_get(dest);
     if let Some(code) = NonZeroU32::new(ret as u32) {
-        error!("WASI: __wasi_random_get failed with return value {}", code);
+        error!("WASI: random_get failed with return value {}", code);
         Err(Error::from(code))
     } else {
         Ok(()) // Zero means success for WASI
