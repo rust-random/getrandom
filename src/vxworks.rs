a2a7b1140e8e99ac0acb97f28c1a05b10685e300
newpavlov <newpavlov@gmail.com>
2019-08-14 18:10:58 +0300

    fix

--- src/vxworks.rs
+++ src/vxworks.rs
@@ -7,9 +7,10 @@
 // except according to those terms.
 
 //! Implementation for VxWorks
-use crate::Error;
+use crate::error::{Error, ERRNO_NOT_POSITIVE};
+use core::num::NonZeroU32;
 
-extern {
+extern "C" {
     fn randBytes (buf: *mut u8, length: i32) -> i32;
     // errnoLib.h
     fn errnoGet() -> i32;
@@ -18,7 +19,7 @@ extern {
 pub fn getrandom_inner(dest: &mut [u8]) -> Result<(), Error> {
     // Prevent overflow of i32
     for chunk in dest.chunks_mut(i32::max_value() as usize) {
-        let ret = randBytes(chunk.as_mut_ptr(), chunk.len());
+        let ret = randBytes(chunk.as_mut_ptr(), chunk.len() as i32);
         if ret == -1 {
             let errno = errnoGet();
             let err = if errno > 0 {
