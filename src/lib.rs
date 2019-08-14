bdf7c942fd87d646e7ed2903dcd9f632b068e1cb
newpavlov <newpavlov@gmail.com>
2019-08-14 17:58:16 +0300

    add vxworks support

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,7 @@
 //! | Haiku            | `/dev/random` (identical to `/dev/urandom`)
 //! | L4RE, SGX, UEFI  | [RDRAND][18]
 //! | Hermit           | [RDRAND][18] as [`sys_rand`][22] is currently broken.
+//! | VxWorks          | `randBytes`
 //! | Web browsers     | [`Crypto.getRandomValues`][14] (see [Support for WebAssembly and ams.js][14])
 //! | Node.js          | [`crypto.randomBytes`][15] (see [Support for WebAssembly and ams.js][16])
 //! | WASI             | [`__wasi_random_get`][17]
@@ -225,6 +226,8 @@ cfg_if! {
         #[path = "solaris_illumos.rs"] mod imp;
     } else if #[cfg(target_os = "wasi")] {
         #[path = "wasi.rs"] mod imp;
+    } else if #[cfg(target_os = "vxworks")] {
+        #[path = "vxworks.rs"] mod imp;
     } else if #[cfg(all(windows, getrandom_uwp))] {
         #[path = "windows_uwp.rs"] mod imp;
     } else if #[cfg(windows)] {
---
+++ src/vxworks.rs
@@ -0,0 +1,33 @@
+// Copyright 2018 Developers of the Rand project.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! Implementation for VxWorks
+use crate::Error;
+
+extern {
+    fn randBytes (buf: *mut u8, length: i32) -> i32;
+    // errnoLib.h
+    fn errnoGet() -> i32;
+}
+
+pub fn getrandom_inner(dest: &mut [u8]) -> Result<(), Error> {
+    // Prevent overflow of i32
+    for chunk in dest.chunks_mut(i32::max_value() as usize) {
+        let ret = randBytes(chunk.as_mut_ptr(), chunk.len());
+        if ret == -1 {
+            let errno = errnoGet();
+            let err = if errno > 0 {
+                Error::from(NonZeroU32::new(errno as u32).unwrap())
+            } else {
+                ERRNO_NOT_POSITIVE
+            };
+            return Err(err);
+        }
+    }
+    Ok(())
+}
