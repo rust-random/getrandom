8e1ab1a3da9a96bed15a6628e43e4fda6cad7035
Joe Richey <joerichey@google.com>
2020-09-08 23:55:20 -0700

    custom: Add check for function type
    This makes sure we get a good compiler error if we give a bad path.
    
    Signed-off-by: Joe Richey <joerichey@google.com>

--- src/custom.rs
+++ src/custom.rs
@@ -78,8 +78,9 @@ macro_rules! register_custom_getrandom {
         // We use an extern "C" function to get the guarantees of a stable ABI.
         #[no_mangle]
         extern "C" fn __getrandom_custom(dest: *mut u8, len: usize) -> u32 {
+            let f: fn(&mut [u8]) -> Result<(), ::getrandom::Error> = $path;
             let slice = unsafe { ::core::slice::from_raw_parts_mut(dest, len) };
-            match $path(slice) {
+            match f(slice) {
                 Ok(()) => 0,
                 Err(e) => e.code().get(),
             }
