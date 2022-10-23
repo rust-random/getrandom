99077a37c126030b59f49b83674a62ebd21cde51
Joe Richey <joerichey@google.com>
2022-10-22 19:43:44 -0700

    Cleanup wasm32-wasi target
    This change ensures that we only compile our WASI implementation for
    32-bit targets. The interaction between the WASI proposal and the
    memory64 proposal is not yet clear, [wasmtime does not yet support](
    https://github.com/bytecodealliance/wasmtime/issues/3594#issuecomment-992590383)
    using WASI with memory64, and many of the interfaces use 32-bit values
    for pointers.
    
    This change also reduces the use of `unsafe` from the wasi
    implementation. As noted in #253, changes to `Errno` mean that we can't
    get the error message from the raw error code, but we can avoid using
    unsafe when converting this code to a NonZeroU32. This handling also
    makes WASI behave more like our other targets, which also manually check
    that errno is non-zero.
    
    Signed-off-by: Joe Richey <joerichey@google.com>

--- src/lib.rs
+++ src/lib.rs
@@ -238,7 +238,7 @@ cfg_if! {
     } else if #[cfg(target_os = "openbsd")] {
         mod util_libc;
         #[path = "openbsd.rs"] mod imp;
-    } else if #[cfg(target_os = "wasi")] {
+    } else if #[cfg(all(target_arch = "wasm32", target_os = "wasi"))] {
         #[path = "wasi.rs"] mod imp;
     } else if #[cfg(all(target_arch = "x86_64", target_os = "hermit"))] {
         #[path = "rdrand.rs"] mod imp;
