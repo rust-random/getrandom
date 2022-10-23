00e447fd70d60bf2505bf8ae1596bb3a5e18d743
Joe Richey <joerichey@google.com>
2022-10-23 00:18:45 -0700

    Document use of /dev/urandom vs /dev/random
    Also, on platforms where the devices are the same, prefer using
    /dev/urandom.
    
    Documentation of the devices being the same:
    - macOS [`random(4)`](https://www.unix.com/man-page/mojave/4/urandom/)
    - DragonFly [`getrandom(2)`](https://leaf.dragonflybsd.org/cgi/web-man?command=getrandom)
    - Haiku [implementation](https://github.com/haiku/haiku/blob/f40bacae87f37276be7d107a6b3d41ca97b4d82c/src/add-ons/kernel/bus_managers/random/driver.cpp#L217-L222)
    
    Signed-off-by: Joe Richey <joerichey@google.com>

--- src/use_file.rs
+++ src/use_file.rs
@@ -18,16 +18,21 @@ use core::{
     sync::atomic::{AtomicUsize, Ordering::Relaxed},
 };
 
+// We prefer using /dev/urandom and only use /dev/random if the OS
+// documentation indicates that /dev/urandom is insecure.
+// On Solaris/Illumos, see src/solaris_illumos.rs
+// On Dragonfly, Haiku, and macOS, the devices are identical.
+#[cfg(any(target_os = "solaris", target_os = "illumos"))]
+const FILE_PATH: &str = "/dev/random\0";
 #[cfg(any(
+    target_os = "android",
+    target_os = "linux",
+    target_os = "redox",
     target_os = "dragonfly",
     target_os = "emscripten",
     target_os = "haiku",
-    target_os = "macos",
-    target_os = "solaris",
-    target_os = "illumos"
+    target_os = "macos"
 ))]
-const FILE_PATH: &str = "/dev/random\0";
-#[cfg(any(target_os = "android", target_os = "linux", target_os = "redox"))]
 const FILE_PATH: &str = "/dev/urandom\0";
 
 pub fn getrandom_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
