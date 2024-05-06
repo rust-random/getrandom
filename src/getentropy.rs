229d87004d33316664fd5c891f96b6b29972c080
Joe Richey <joerichey@google.com>
2024-05-06 05:10:37 -0700

    Use libc::getrandom on Solaris and update docs. (#420)
    https://github.com/rust-random/getrandom/pull/417 used `getentropy(2)`
    on Solaris, but after looking at
    [the blog post introducing `getrandom()` and `getentropy()`](https://blogs.oracle.com/solaris/post/solaris-new-system-calls-getentropy2-and-getrandom2),
    it seems like we should prefer using `getrandom` based on this quote:
    
    > On Solaris the output of getentropy(2) is entropy and should not be used where randomness is needed, in particular it must not be used where an IV or nonce is needed when calling a cryptographic operation.  It is intended only for seeding a user space RBG (Random Bit Generator) system. More specifically the data returned by getentropy(2) has not had the required FIPS 140-2 processing for the DRBG applied to it.
    
    I also updated some of the documentation explaining:
      - Why we use `getentropy(2)`
      - Why we only set `GRND_RANDOM` on Solaris
    
    Signed-off-by: Joe Richey <joerichey@google.com>

--- src/getentropy.rs
+++ src/getentropy.rs
@@ -1,10 +1,12 @@
-//! Implementation using libc::getentropy
+//! Implementation using getentropy(2)
 //!
 //! Available since:
 //!   - macOS 10.12
 //!   - OpenBSD 5.6
 //!   - Emscripten 2.0.5
 //!   - vita newlib since Dec 2021
+//!
+//! For these targets, we use getentropy(2) because getrandom(2) doesn't exist.
 use crate::{util_libc::last_os_error, Error};
 use core::mem::MaybeUninit;
 
