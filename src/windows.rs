c231147602f5e2e90b44e1a27568d3441eabf35b
Joe Richey <joerichey@google.com>
2024-05-28 20:51:33 -0700

    Update documentation and don't use newtype for BOOL/BOOLEAN
    Signed-off-by: Joe Richey <joerichey@google.com>

--- src/windows.rs
+++ src/windows.rs
@@ -1,35 +1,33 @@
 //! Implementation for Windows 10 and later
 //!
 //! On Windows 10 and later, ProcessPrng "is the primary interface to the
-//! user-mode per-processer PRNGs" and only requires BCryptPrimitives.dll,
+//! user-mode per-processer PRNGs" and only requires bcryptprimitives.dll,
 //! making it a better option than the other Windows RNG APIs:
 //!   - BCryptGenRandom: https://learn.microsoft.com/en-us/windows/win32/api/bcrypt/nf-bcrypt-bcryptgenrandom
-//!     - Requires Bcrypt.dll (which loads BCryptPrimitives.dll anyway)
+//!     - Requires bcrypt.dll (which loads bcryptprimitives.dll anyway)
 //!     - Can cause crashes/hangs as BCrypt accesses the Windows Registry:
 //!       https://github.com/rust-lang/rust/issues/99341
 //!     - Causes issues inside sandboxed code:
 //!       https://issues.chromium.org/issues/40277768
 //!   - CryptGenRandom: https://learn.microsoft.com/en-us/windows/win32/api/wincrypt/nf-wincrypt-cryptgenrandom
 //!     - Deprecated and not available on UWP targets
-//!     - Requires Advapi32.lib/Advapi32.dll
-//!     - Wrapper around ProcessPrng
+//!     - Requires advapi32.lib/advapi32.dll (in addition to bcryptprimitives.dll)
+//!     - Thin wrapper around ProcessPrng
 //!   - RtlGenRandom: https://learn.microsoft.com/en-us/windows/win32/api/ntsecapi/nf-ntsecapi-rtlgenrandom
 //!     - Deprecated and not available on UWP targets
-//!     - Requires Advapi32.dll (and using name "SystemFunction036")
-//!     - Wrapper around ProcessPrng
+//!     - Requires advapi32.dll (in addition to bcryptprimitives.dll)
+//!     - Requires using name "SystemFunction036"
+//!     - Thin wrapper around ProcessPrng
 //! For more information see the Windows RNG Whitepaper: https://aka.ms/win10rng
 use crate::Error;
 use core::mem::MaybeUninit;
 
-// ProcessPrng lacks an import library, so we use the windows-targets crate to
-// link to it. The following code was generated via windows-bindgen with APIs:
-//   Windows.Win32.Foundation.TRUE
-//   Windows.Win32.Security.Cryptography.ProcessPrng
+// Binding to the Windows.Win32.Security.Cryptography.ProcessPrng API. As
+// bcryptprimitives.dll lacks an import library, we use the windows-targets
+// crate to link to it.
 windows_targets::link!("bcryptprimitives.dll" "system" fn ProcessPrng(pbdata: *mut u8, cbdata: usize) -> BOOL);
-#[repr(transparent)]
-#[derive(PartialEq, Eq)]
-pub struct BOOL(pub i32);
-pub const TRUE: BOOL = BOOL(1i32);
+pub type BOOL = i32;
+pub const TRUE: BOOL = 1i32;
 
 pub fn getrandom_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
     // ProcessPrng should always return TRUE, but we check just in case.
--- src/windows7.rs
+++ src/windows7.rs
@@ -1,29 +1,26 @@
-//! Implementation for Windows 7 and 8
+//! Legacy implementation for Windows XP and later
 //!
 //! For targets where we cannot use ProcessPrng (added in Windows 10), we use
 //! RtlGenRandom. See windows.rs for a more detailed discussion of the Windows
 //! RNG APIs (and why we don't use BCryptGenRandom). On versions prior to
-//! Windows 10, this implementation works, while on Windows 10 and later, this
-//! is a thin wrapper around ProcessPrng.
+//! Windows 10, this implementation is secure. On Windows 10 and later, this
+//! implementation behaves identically to the windows.rs implementation, except
+//! that it forces the loading of an additonal DLL (advapi32.dll).
 //!
-//! This implementation will not work on UWP targets, but those targets require
-//! Windows 10 regardless, so can use the ProcessPrng implementation.
+//! This implementation will not work on UWP targets (which lack advapi32.dll),
+//! but such targets require Windows 10, so can use the standard implementation.
 use crate::Error;
 use core::{ffi::c_void, mem::MaybeUninit};
 
-// This code is based on that produced by windows-bindgen with the APIs:
-//   Windows.Win32.Foundation.TRUE
-//   Windows.Win32.Security.Authentication.Identity.RtlGenRandom
-// but we avoid using windows-targets as it doesn't support older Windows.
+// Binding to the Windows.Win32.Security.Authentication.Identity.RtlGenRandom
+// API. Don't use windows-targets as it doesn't support Windows 7 targets.
 #[link(name = "advapi32")]
 extern "system" {
     #[link_name = "SystemFunction036"]
     fn RtlGenRandom(randombuffer: *mut c_void, randombufferlength: u32) -> BOOLEAN;
 }
-#[repr(transparent)]
-#[derive(PartialEq, Eq)]
-pub struct BOOLEAN(pub u8);
-pub const TRUE: BOOLEAN = BOOLEAN(1u8);
+type BOOLEAN = u8;
+const TRUE: BOOLEAN = 1u8;
 
 pub fn getrandom_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
     // Prevent overflow of u32
