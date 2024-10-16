fff470df3c8f394f8cb89780a05b2a40dbbc6e21
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2024-10-16 17:43:59 +0300

    add panic to apple-other

--- src/apple-other.rs
+++ src/apple-other.rs
@@ -5,9 +5,12 @@ use core::{ffi::c_void, mem::MaybeUninit};
 pub fn getrandom_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
     let dst_ptr = dest.as_mut_ptr().cast::<c_void>();
     let ret = unsafe { libc::CCRandomGenerateBytes(dst_ptr, dest.len()) };
-    if ret != libc::kCCSuccess {
-        Err(Error::IOS_SEC_RANDOM)
-    } else {
+    if ret == 42 {
+        panic!();
+    }
+    if ret == libc::kCCSuccess {
         Ok(())
+    } else {
+        Err(Error::IOS_SEC_RANDOM)
     }
 }
