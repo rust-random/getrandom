b0e97c11444a04825d8d0d27841b3c5341c12d2c
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2024-11-25 19:59:10 +0300

    Use `sys_secure_rand32/64` on Hermit

--- src/backends/hermit.rs
+++ src/backends/hermit.rs
@@ -2,12 +2,34 @@
 use crate::Error;
 use core::mem::MaybeUninit;
 
-// Note that `sys_secure_rand32` and `sys_secure_rand64` are implemented using `sys_read_entropy`:
-// https://docs.rs/libhermit-rs/0.6.3/src/hermit/syscalls/entropy.rs.html#62-97
-pub use crate::util::{inner_u32, inner_u64};
-
 extern "C" {
     fn sys_read_entropy(buffer: *mut u8, length: usize, flags: u32) -> isize;
+    // Note that `sys_secure_rand32/64` are implemented using `sys_read_entropy`:
+    // https://github.com/hermit-os/kernel/blob/430da84/src/syscalls/entropy.rs#L62-L104
+    // But this may change in future and can depend on compilation target,
+    // so to future-proof we use these "syscalls".
+    fn sys_secure_rand32(value: *mut u32) -> i32;
+    fn sys_secure_rand64(value: *mut u64) -> i32;
+}
+
+pub fn inner_u32() -> Result<u32, Error> {
+    let mut res = MaybeUninit::uninit();
+    let ret = unsafe { sys_secure_rand32(res.as_mut_ptr()) };
+    match ret {
+        0 => Ok(unsafe { res.assume_init() }),
+        -1 => Err(Error::UNSUPPORTED),
+        _ => Err(Error::UNEXPECTED),
+    }
+}
+
+pub fn inner_u64() -> Result<u64, Error> {
+    let mut res = MaybeUninit::uninit();
+    let ret = unsafe { sys_secure_rand64(res.as_mut_ptr()) };
+    match ret {
+        0 => Ok(unsafe { res.assume_init() }),
+        -1 => Err(Error::UNSUPPORTED),
+        _ => Err(Error::UNEXPECTED),
+    }
 }
 
 pub fn fill_inner(mut dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
