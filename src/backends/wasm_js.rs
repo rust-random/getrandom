1dbda17e674d147af169d09f269a523f1eb03112
daxpedda <daxpedda@gmail.com>
2024-12-05 00:13:53 +0100

    Cache global `Crypto` object

--- src/backends/wasm_js.rs
+++ src/backends/wasm_js.rs
@@ -7,52 +7,47 @@ pub use crate::util::{inner_u32, inner_u64};
 #[cfg(not(all(target_arch = "wasm32", any(target_os = "unknown", target_os = "none"))))]
 compile_error!("`wasm_js` backend can be enabled only for OS-less WASM targets!");
 
-use js_sys::{global, Uint8Array};
-use wasm_bindgen::{prelude::wasm_bindgen, JsCast, JsValue};
+use js_sys::Uint8Array;
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
 // Size of our temporary Uint8Array buffer used with WebCrypto methods
 // Maximum is 65536 bytes see https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
 const CRYPTO_BUFFER_SIZE: u16 = 256;
 
 pub fn fill_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
-    let global: Global = global().unchecked_into();
-    let crypto = global.crypto();
+    CRYPTO.with(|crypto| {
+        let crypto = crypto.as_ref().ok_or(Error::WEB_CRYPTO)?;
 
-    if !crypto.is_object() {
-        return Err(Error::WEB_CRYPTO);
-    }
+        // getRandomValues does not work with all types of WASM memory,
+        // so we initially write to browser memory to avoid exceptions.
+        let buf = Uint8Array::new_with_length(CRYPTO_BUFFER_SIZE.into());
+        for chunk in dest.chunks_mut(CRYPTO_BUFFER_SIZE.into()) {
+            let chunk_len: u32 = chunk
+                .len()
+                .try_into()
+                .expect("chunk length is bounded by CRYPTO_BUFFER_SIZE");
+            // The chunk can be smaller than buf's length, so we call to
+            // JS to create a smaller view of buf without allocation.
+            let sub_buf = buf.subarray(0, chunk_len);
 
-    // getRandomValues does not work with all types of WASM memory,
-    // so we initially write to browser memory to avoid exceptions.
-    let buf = Uint8Array::new_with_length(CRYPTO_BUFFER_SIZE.into());
-    for chunk in dest.chunks_mut(CRYPTO_BUFFER_SIZE.into()) {
-        let chunk_len: u32 = chunk
-            .len()
-            .try_into()
-            .expect("chunk length is bounded by CRYPTO_BUFFER_SIZE");
-        // The chunk can be smaller than buf's length, so we call to
-        // JS to create a smaller view of buf without allocation.
-        let sub_buf = buf.subarray(0, chunk_len);
+            if crypto.get_random_values(&sub_buf).is_err() {
+                return Err(Error::WEB_GET_RANDOM_VALUES);
+            }
 
-        if crypto.get_random_values(&sub_buf).is_err() {
-            return Err(Error::WEB_GET_RANDOM_VALUES);
+            // SAFETY: `sub_buf`'s length is the same length as `chunk`
+            unsafe { sub_buf.raw_copy_to_ptr(chunk.as_mut_ptr().cast::<u8>()) };
         }
-
-        // SAFETY: `sub_buf`'s length is the same length as `chunk`
-        unsafe { sub_buf.raw_copy_to_ptr(chunk.as_mut_ptr().cast::<u8>()) };
-    }
-    Ok(())
+        Ok(())
+    })
 }
 
 #[wasm_bindgen]
 extern "C" {
-    // Return type of js_sys::global()
-    type Global;
     // Web Crypto API: Crypto interface (https://www.w3.org/TR/WebCryptoAPI/)
     type Crypto;
-    // Getters for the Crypto API
-    #[wasm_bindgen(method, getter)]
-    fn crypto(this: &Global) -> Crypto;
+    // Holds the global `Crypto` object.
+    #[wasm_bindgen(thread_local_v2, js_name = crypto)]
+    static CRYPTO: Option<Crypto>;
     // Crypto.getRandomValues()
     #[wasm_bindgen(method, js_name = getRandomValues, catch)]
     fn get_random_values(this: &Crypto, buf: &Uint8Array) -> Result<(), JsValue>;
