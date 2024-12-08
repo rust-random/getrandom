65ba72733cfdb41859ea17c24de0be46e93fc870
daxpedda <daxpedda@gmail.com>
2024-12-08 10:36:57 +0100

    Fix `MAX_BUFFER_SIZE`

--- src/backends/wasm_js.rs
+++ src/backends/wasm_js.rs
@@ -13,7 +13,7 @@ use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
 // Maximum buffer size allowed in `Crypto.getRandomValuesSize` is 65536 bytes.
 // See https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
-const MAX_BUFFER_SIZE: u16 = 256;
+const MAX_BUFFER_SIZE: u32 = 65536;
 
 pub fn fill_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
     CRYPTO.with(|crypto| {
@@ -24,7 +24,7 @@ pub fn fill_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
 
 #[cfg(not(target_feature = "atomics"))]
 fn inner(crypto: &Crypto, dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
-    for chunk in dest.chunks_mut(MAX_BUFFER_SIZE.into()) {
+    for chunk in dest.chunks_mut(MAX_BUFFER_SIZE as usize) {
         if crypto.get_random_values(chunk).is_err() {
             return Err(Error::WEB_GET_RANDOM_VALUES);
         }
@@ -36,15 +36,19 @@ fn inner(crypto: &Crypto, dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
 fn inner(crypto: &Crypto, dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
     // getRandomValues does not work with all types of WASM memory,
     // so we initially write to browser memory to avoid exceptions.
-    let buf = Uint8Array::new_with_length(MAX_BUFFER_SIZE.into());
-    for chunk in dest.chunks_mut(MAX_BUFFER_SIZE.into()) {
+    let buf_len = u32::min(
+        dest.len().try_into().unwrap_or(MAX_BUFFER_SIZE),
+        MAX_BUFFER_SIZE,
+    );
+    let buf = Uint8Array::new_with_length(buf_len);
+    for chunk in dest.chunks_mut(buf_len as usize) {
         let chunk_len: u32 = chunk
             .len()
             .try_into()
             .expect("chunk length is bounded by MAX_BUFFER_SIZE");
         // The chunk can be smaller than buf's length, so we call to
         // JS to create a smaller view of buf without allocation.
-        let sub_buf = if chunk_len == u32::from(MAX_BUFFER_SIZE) {
+        let sub_buf = if chunk_len == buf_len {
             &buf
         } else {
             &buf.subarray(0, chunk_len)
