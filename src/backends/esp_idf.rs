2648e60c5d27979f45043e379b1af7bee5e300a9
Joe Richey <joerichey@google.com>
2025-01-13 06:43:54 -0800

    ESP-IDF: Enable unconditionally (#583)
    After reviewing the updated ESP-IDF random documentation:
    -
    https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/random.html
    -
    https://www.espressif.com/sites/default/files/documentation/esp32_technical_reference_manual_en.pdf#rng
    
    Given that ESP-IDF provides binding of `getrandom`/`getentropy` to
    `esp_fill_random`, it's clear they intend for
    cryptographic libraries (such as their fork of WolfSSL) to use them.
    Furthermore, it seems like the only time the Hardware RNG would lack
    sufficient entropy is during early boot, so I added a section to our
    "Early boot" documentation noting this issue.
    
    Also note that Rust's standard library unconditionally supports ESP-IDF
    for both hash seed generation and generating cryptographic random bytes,
    see
    https://github.com/rust-lang/rust/blob/62bf38fa600f4beb878d61c537837729d4ee689e/library/std/src/sys/random/espidf.rs#L7
    I don't think there's a good reason to deviate from the standard library
    here.

--- src/backends/esp_idf.rs
+++ src/backends/esp_idf.rs
@@ -4,9 +4,6 @@ use core::{ffi::c_void, mem::MaybeUninit};
 
 pub use crate::util::{inner_u32, inner_u64};
 
-#[cfg(not(target_os = "espidf"))]
-compile_error!("`esp_idf` backend can be enabled only for ESP-IDF targets!");
-
 extern "C" {
     fn esp_fill_random(buf: *mut c_void, len: usize) -> u32;
 }
