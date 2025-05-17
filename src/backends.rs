b7384b7e6f0a581229e6e3a0379d2b6d12c54b25
David Sherret <dsherret@gmail.com>
2025-05-17 15:03:50 -0400

    Add runtime_error backend

--- src/backends.rs
+++ src/backends.rs
@@ -38,6 +38,9 @@ cfg_if! {
                 ));
             }
         }
+    } else if #[cfg(getrandom_backend = "runtime_error")] {
+        mod runtime_error;
+        pub use runtime_error::*;
     } else if #[cfg(all(target_os = "linux", target_env = ""))] {
         mod linux_raw;
         pub use linux_raw::*;
