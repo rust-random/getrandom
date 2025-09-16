cf9d5dfabf7dd9b29339a93ad944d1c63109335a
Artyom Pavlov <newpavlov@gmail.com>
2025-09-16 11:16:28 +0300

    Replace `wasi` crate with `wasip2` (#721)
    The recent releases of `wasi` simply re-export contents of `wasip2`.

--- src/backends/wasi_p2.rs
+++ src/backends/wasi_p2.rs
@@ -1,7 +1,7 @@
 //! Implementation for WASI Preview 2.
 use crate::Error;
-use core::mem::MaybeUninit;
-use wasi::random::random::get_random_u64;
+use core::{mem::MaybeUninit, ptr::copy_nonoverlapping};
+use wasip2::random::random::get_random_u64;
 
 #[inline]
 pub fn inner_u32() -> Result<u32, Error> {
@@ -16,9 +16,6 @@ pub fn inner_u64() -> Result<u64, Error> {
 
 #[inline]
 pub fn fill_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
-    use core::ptr::copy_nonoverlapping;
-    use wasi::random::random::get_random_u64;
-
     let (prefix, chunks, suffix) = unsafe { dest.align_to_mut::<MaybeUninit<u64>>() };
 
     // We use `get_random_u64` instead of `get_random_bytes` because the latter creates
