6254a329abfa3658cda359f4172b980ff79b8feb
Diggory Hardy <git@dhardy.name>
2025-09-22 10:15:15 +0100

    Let the wasm_js feature assume JS as a fallback

--- src/backends.rs
+++ src/backends.rs
@@ -31,7 +31,7 @@ cfg_if! {
     } else if #[cfg(getrandom_backend = "windows_legacy")] {
         mod windows_legacy;
         pub use windows_legacy::*;
-    } else if #[cfg(all(getrandom_backend = "wasm_js"))] {
+    } else if #[cfg(getrandom_backend = "wasm_js")] {
         cfg_if! {
             if #[cfg(feature = "wasm_js")] {
                 mod wasm_js;
@@ -186,13 +186,20 @@ cfg_if! {
         mod rdrand;
         pub use rdrand::*;
     } else if #[cfg(all(target_arch = "wasm32", any(target_os = "unknown", target_os = "none")))] {
-        compile_error!(concat!(
-            "The wasm32-unknown-unknown targets are not supported by default; \
-            you may need to enable the \"wasm_js\" configuration flag. Note \
-            that enabling the `wasm_js` feature flag alone is insufficient. \
-            For more information see: \
-            https://docs.rs/getrandom/", env!("CARGO_PKG_VERSION"), "/#webassembly-support"
-        ));
+        cfg_if! {
+            if #[cfg(feature = "wasm_js")] {
+                mod wasm_js;
+                pub use wasm_js::*;
+            } else {
+                compile_error!(concat!(
+                    "The wasm32-unknown-unknown targets are not supported by default; \
+                    you may need to enable the \"wasm_js\" configuration flag. Note \
+                    that enabling the `wasm_js` feature flag alone is insufficient. \
+                    For more information see: \
+                    https://docs.rs/getrandom/", env!("CARGO_PKG_VERSION"), "/#webassembly-support"
+                ));
+            }
+        }
     } else {
         compile_error!(concat!(
             "target is not supported. You may need to define a custom backend see: \
