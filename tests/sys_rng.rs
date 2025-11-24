4275e2563814faa13ccc87753c567d03997c6873
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2025-11-24 14:53:23 +0300

    Add `UnwrappingSysRng`

--- tests/sys_rng.rs
+++ tests/sys_rng.rs
@@ -1,17 +1,57 @@
 #![cfg(feature = "sys_rng")]
 
-use getrandom::SysRng;
-use getrandom::rand_core::TryRngCore;
+use core::array::from_fn;
+use getrandom::{
+    SysRng, UnwrappingSysRng,
+    rand_core::{RngCore, TryRngCore},
+};
+
+const N: usize = 32;
 
 #[test]
 fn test_sys_rng() {
-    let x = SysRng.try_next_u64().unwrap();
-    let y = SysRng.try_next_u64().unwrap();
-    assert!(x != 0);
+    let x: [u64; N] = from_fn(|_| SysRng.try_next_u64().unwrap());
+    let y: [u64; N] = from_fn(|_| SysRng.try_next_u64().unwrap());
+    assert!(x.iter().all(|&val| val != 0));
+    assert!(y.iter().all(|&val| val != 0));
+    assert!(x != y);
+
+    let x: [u32; N] = from_fn(|_| SysRng.try_next_u32().unwrap());
+    let y: [u32; N] = from_fn(|_| SysRng.try_next_u32().unwrap());
+    assert!(x.iter().all(|&val| val != 0));
+    assert!(y.iter().all(|&val| val != 0));
+    assert!(x != y);
+
+    let mut x = [0u8; N];
+    SysRng.try_fill_bytes(&mut x).unwrap();
+    let mut y = [0u8; N];
+    SysRng.try_fill_bytes(&mut y).unwrap();
+
+    assert_ne!(x, [0; N]);
+    assert_ne!(y, [0; N]);
     assert!(x != y);
 }
 
 #[test]
-fn test_construction() {
-    assert!(SysRng.try_next_u64().unwrap() != 0);
+fn test_unwrapping_sys_rng() {
+    let x: [u64; N] = from_fn(|_| UnwrappingSysRng.next_u64());
+    let y: [u64; N] = from_fn(|_| UnwrappingSysRng.next_u64());
+    assert!(x.iter().all(|&val| val != 0));
+    assert!(y.iter().all(|&val| val != 0));
+    assert!(x != y);
+
+    let x: [u32; N] = from_fn(|_| UnwrappingSysRng.next_u32());
+    let y: [u32; N] = from_fn(|_| UnwrappingSysRng.next_u32());
+    assert!(x.iter().all(|&val| val != 0));
+    assert!(y.iter().all(|&val| val != 0));
+    assert!(x != y);
+
+    let mut x = [0u8; N];
+    UnwrappingSysRng.try_fill_bytes(&mut x);
+    let mut y = [0u8; N];
+    UnwrappingSysRng.try_fill_bytes(&mut y);
+
+    assert_ne!(x, [0; N]);
+    assert_ne!(y, [0; N]);
+    assert!(x != y);
 }
