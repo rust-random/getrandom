37a1ab8284a395e8844080ab59b20c6a2c9ace52
Zac Harrold <zac@harrold.com.au>
2026-01-27 09:57:42 +1100

    Move implementation docs

--- src/lib.rs
+++ src/lib.rs
@@ -35,30 +35,29 @@ pub use sys_rng::SysRng;
 
 pub use crate::error::{Error, RawOsError};
 
+/// Provides Externally Implementable Interfaces for the core functionality of this crate.
+/// This allows `getrandom` to provide a default implementation and a common interface
+/// for all crates to use, while giving users a safe way to override that default where required.
+///
+/// Must be enabled via the `extern_item_impls` opt-in backend, as this functionality
+/// is currently limited to nightly.
+///
+/// # Examples
+///
+/// ```rust
+/// # use core::mem::MaybeUninit;
+/// # #[cfg(getrandom_backend = "extern_item_impls")]
+/// #[getrandom::implementation::fill_uninit]
+/// fn my_fill_uninit_implementation(
+///     dest: &mut [MaybeUninit<u8>]
+/// ) -> Result<(), getrandom::Error> {
+///     // ...
+/// #   let _ = dest;
+/// #   Err(Error::UNSUPPORTED)
+/// }
+/// ```
 #[cfg(getrandom_backend = "extern_item_impls")]
 pub mod implementation {
-    //! Provides Externally Implementable Interfaces for the core functionality of this crate.
-    //! This allows `getrandom` to provide a default implementation and a common interface
-    //! for all crates to use, while giving users a safe way to override that default where required.
-    //!
-    //! Must be enabled via the `extern_item_impls` opt-in backend, as this functionality
-    //! is currently limited to nightly.
-    //!
-    //! # Examples
-    //!
-    //! ```rust
-    //! # use core::mem::MaybeUninit;
-    //! # #[cfg(getrandom_backend = "extern_item_impls")]
-    //! #[getrandom::implementation::fill_uninit]
-    //! fn my_fill_uninit_implementation(
-    //!     dest: &mut [MaybeUninit<u8>]
-    //! ) -> Result<(), getrandom::Error> {
-    //!     // ...
-    //! #   let _ = dest;
-    //! #   Err(Error::UNSUPPORTED)
-    //! }
-    //! ```
-
     pub use crate::backends::extern_item_impls::{fill_uninit, u32, u64};
 }
 
