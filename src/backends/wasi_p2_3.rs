422b7241042179c55688359b2c6ea8fa2eda8230
Alex Crichton <alex@alexcrichton.com>
2026-03-09 08:10:18 -0700

    Check-in pregenerated WASIp2/3 bindings
    This commit updates the backends for the `wasm32-wasip{2,3}` targets to
    skip using the `wasip2` and `wasip3` crates and instead use generated
    bindings directly. This avoids the dependency on the `wasip2` and
    `wasip3` crates which transitively depend on `wit-bindgen`. This then
    additionally avoids a Cargo bug where although `getrandom` never
    activates some features of `wit-bindgen` all the optional dependencies
    of `wit-bindgen` are included in downstream `Cargo.lock` files.
    
    A CI job is added which regenerates the bindings and ensures that
    everything is up-to-date to enforce that the state in-repo is fresh at
    all times. Some small workarounds were necessary in the generated
    bindings to force the custom sections to get emitted correctly, but this
    reflects what the upstream crates/bindings are already doing and is
    necessary to replicate here with checked in bindings for now.

--- src/backends/wasi_p2_3.rs
+++ src/backends/wasi_p2_3.rs
@@ -3,32 +3,58 @@ use crate::Error;
 use core::{mem::MaybeUninit, ptr::copy_nonoverlapping};
 
 #[cfg(target_env = "p2")]
-use wasip2 as wasi;
+mod p2 {
+    include!("./wasi_p2_3/p2/imports.rs");
+    #[inline(never)]
+    pub fn _link_custom_section_describing_imports() {}
+}
+#[cfg(target_env = "p2")]
+use p2::*;
 
 // Workaround to silence `unexpected_cfgs` warning
 // on Rust version between 1.85 and 1.91
 #[cfg(not(target_env = "p2"))]
 #[cfg(target_env = "p3")]
-use wasip3 as wasi;
+mod p3 {
+    include!("./wasi_p2_3/p3/imports.rs");
+    #[inline(never)]
+    pub fn _link_custom_section_describing_imports() {}
+}
+#[cfg(not(target_env = "p2"))]
+#[cfg(target_env = "p3")]
+use p3::*;
 
 #[cfg(not(target_env = "p2"))]
 #[cfg(not(target_env = "p3"))]
 compile_error!("Unknown version of WASI (only previews 1, 2 and 3 are supported)");
 
+// This is a bit subtle, in addition to the `include!` above, but the general
+// idea is that `wit-bindgen` generates a custom section of type information
+// needed by `wasm-component-ld`. That needs to make its way to the linker and
+// that's particularly tricky to do unfortunately. To force the linker to at
+// least witness the object file with the custom section in it a `#[used]`
+// static here refers to a function in the `p2`/`p3` modules which is adjacent
+// to the custom section. This is then coupled with a lack of `#[inline]` below
+// such that whenever this file itself is used it'll force the linker to look at
+// this `#[used]` static, then look at the type section, and include that.
+//
+// Note that the linker will strip this static as well as the referred-to
+// function as it's actually dead, but the linker will still preserve the type
+// information since it's in a custom section, which is what we want.
+#[used]
+static _FORCE_SECTION_REF: fn() = _link_custom_section_describing_imports;
+
 use wasi::random::random::get_random_u64;
 
-#[inline]
 pub fn inner_u32() -> Result<u32, Error> {
     let val = get_random_u64();
     Ok(crate::util::truncate(val))
 }
 
-#[inline]
 pub fn inner_u64() -> Result<u64, Error> {
     Ok(get_random_u64())
 }
 
-#[inline]
 pub fn fill_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
     let (prefix, chunks, suffix) = unsafe { dest.align_to_mut::<MaybeUninit<u64>>() };
 
